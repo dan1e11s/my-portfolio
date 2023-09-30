ded98b047f92e98f4bbe5cd066515b06cb0ff742
dan1e11s <danielmuratov2003@gmail.com>
2023-09-30 21:08:10 +0600

    added header and started intro block

---
+++ components/intro.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+
+import { motion } from 'framer-motion';
+
+import avatar from '@/public/ava.jpg';
+
+export default function Intro() {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'tween',
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src={avatar}
+              alt="avatar"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="w-24 h-24 object-cover rounded-full border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+    </section>
+  );
+}
