fbc0393f2520d77a9dbbcd7d54739dca017429a5
dan1e11s <danielmuratov2003@gmail.com>
2023-10-01 15:18:31 +0600

    finished intro block

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,15 +1,22 @@
 'use client';
 
 import React from 'react';
+
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { motion } from 'framer-motion';
+
+import { BsArrowRight } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithubSquare } from 'react-icons/fa';
 
 import avatar from '@/public/ava.jpg';
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28px max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -45,6 +52,56 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Daniel.</span> I'm a{' '}
+        <span className="font-bold">frontend developer</span> with{' '}
+        <span className="font-bold">1 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{' '}
+        <span className="underline">React (Next.js) and Svelte</span>
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{' '}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a
+          href="/cv.pdf"
+          download
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          Download CV{' '}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/daniel-muratov-117682249/"
+          target="_blank"
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/dan1e11s"
+          target="_blank"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
