dfe97b7f606d8ab541feec0132aa5d30573e9a84
dan1e11s <danielmuratov2003@gmail.com>
2023-10-21 21:36:05 +0600

    added react context

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 import Header from '@/components/header';
+import ActiveSectionContextProvider from '@/context/active-section-context';
 
 export const metadata: Metadata = {
   title: 'Daniel | Personal Portfolio',
@@ -23,8 +24,10 @@ export default function RootLayout({
       >
         <div className="bg-[#fbe2e2] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-        <Header />
-        {children}
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+        </ActiveSectionContextProvider>
       </body>
     </html>
   );
